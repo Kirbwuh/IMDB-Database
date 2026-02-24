a83c86f753e9ca528ffe75f70fcceadec0edade3
christopher <christopherlassota@outlook.com>
2026-02-24 14:07:59 -0700

    create movie class with constructor

--- src/CPSC219W26Project.java
+++ src/CPSC219W26Project.java
@@ -1,3 +1,31 @@
+package src;
+import java.util.ArrayList;
+
 public class CPSC219W26Project {
+    public static void main(String[] args) {
+
+        ArrayList<ArrayList<Movie>> movies = new ArrayList();
+    }
+
+    // Movie object
+    public static class Movie {
+        // class structure
+        String title;
+        int date;
+        String description;
+        int rating;
+
+        // constructor to build new movie objects
+        public Movie(String title, int date, String description, int rating) {
+            this.title = title;
+            this.date = date;
+            this.description = description;
+            this.rating = rating;
+        }
+    }
+
+    public static void addNewMovie(String title, int date, String description, int rating) {
+
+    }
 }
 
