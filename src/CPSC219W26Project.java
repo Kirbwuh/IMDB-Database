f350f1b170b9e89a5fd53e788c0292bccc175bd5
J.J. Rondon <82114258+Joaron4@users.noreply.github.com>
2026-02-28 14:14:02 -0700

    deleted already defined class variables

--- src/CPSC219W26Project.java
+++ src/CPSC219W26Project.java
@@ -113,18 +113,7 @@ public class CPSC219W26Project {
         System.exit(0);
     }
 
-    //***********************************************************************
-    //----------------CONSTANTS---------------------------------------------
-    //***********************************************************************
-
-    public static final int SERIES_TITLE = 0;    // Name of movie (String)
-    public static final int RELEASE_YEAR = 1;   // Year movie was released (int)
-    public static final int CERTIFICATION = 2;   // Age / Content Rating PG-13 (boolean)
-    public static final int GENRE = 3;           // Genre(s) of the movies (String)
-    public static final int IMDB_RATING = 4;     // Rating of movie on IMDB (double)
-    public static final int OVERVIEW = 5;        // Short description of movie (String)
-    public static final int DIRECTOR = 6;      // Name of director (String)
-    public static final int GROSS = 7;        // money made by movie (double)
+        // money made by movie (double)
 
     //***********************************************************************
     //----------------PROGRAM INPUT-----------------------------------------
