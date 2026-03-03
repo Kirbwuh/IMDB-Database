5fe9c698cf33a2050d8bfbd6fd0def9f563ca09a
Kirbruh <manasedw@gmail.com>
2026-03-03 11:59:45 -0700

    Added method to calculate and output rating average.

---
+++ tests/getRatingAverageTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.junit.Test;
+
+public class getRatingAverageTest {
+
+    @Test
+    public void getRatingAverage_True(){
+
+    }
+
+    @Test
+    public void getRatingAverage_False(){
+
+    }
+}
