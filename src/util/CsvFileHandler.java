21d04759f2265312c0212ffc8b1558b4352c4db1
Delsune <deltarad.theta@gmai.com>
2026-03-18 10:10:44 -0600

    Created a .csv file and added constructor + one method.

--- src/util/CsvFileHandler.java
+++ src/util/CsvFileHandler.java
@@ -1,4 +1,17 @@
 package util;
 
+/**
+ * Handles saving and loading of comma separated value files.
+ * @author Haitam Lembaid 18/03/2026 T10
+ */
 public class CsvFileHandler {
+    private String filepath;
+
+    public CsvFileHandler(String filepath) {
+        this.filepath;
+    }
+
+    public void saveToCSV(String filepath, model.MovieDatabase db) {
+
+    }
 }
