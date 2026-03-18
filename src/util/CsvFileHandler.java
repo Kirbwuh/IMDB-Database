f0b7e76461a668bec4ecbe79ef1ad8969300bf1f
Delsune <deltarad.theta@gmai.com>
2026-03-18 10:21:29 -0600

    Finished setting up all methods and javadocs.

--- src/util/CsvFileHandler.java
+++ src/util/CsvFileHandler.java
@@ -1,17 +1,42 @@
-package util;
+package src.util;
+
+import  model.Movie;
 
 /**
  * Handles saving and loading of comma separated value files.
  * @author Haitam Lembaid 18/03/2026 T10
  */
 public class CsvFileHandler {
+    // Instance Variables
     private String filepath;
+    private model.Movie movies;
 
-    public CsvFileHandler(String filepath) {
+    /**
+     *
+     * @param filepath
+     * @param movies
+     */
+    public CsvFileHandler(String filepath, model.Movie movies) {
         this.filepath;
+        this.movies;
     }
 
+    /**
+     *
+     * @param filepath
+     * @param db
+     */
     public void saveToCSV(String filepath, model.MovieDatabase db) {
 
+    }
+
+    /**
+     *
+     * @param filepath
+     * @param db
+     * @return
+     */
+    public Movie getMovies(String filepath, model.MovieDatabase db) {
+        return movies;
     }
 }
