101e1121ac7aeeb6c8d63a794d3f77c04c5009bf
christopher <christopherlassota@outlook.com>
2026-03-18 09:51:17 -0600

    create HashMap and addMovie setter

--- src/model/MovieDatabase.java
+++ src/model/MovieDatabase.java
@@ -1,4 +1,37 @@
-package model;
+package Model;
+import java.util.HashMap;
+import model.Movie;
+
+// Replace the static HashMap with a proper OO class. Store List. Manage auto-increment ID.
+//
+//Create MovieDatabase class with private ArrayList and nextId counter
+//
+//addMovie(Movie m) assigns and stores ID
+//
+//removeById(int id)
+//
+//updateById(int id, int field, String val) — keep field index constants
+//
+//getById(int id) returns Movie or null
+//
+//getByTitle(String t) returns Movie or null
+//
+//getAllMovies() returns unmodifiable list
+//
+//Javadoc on all public methods
 
 public class MovieDatabase {
+
+    private HashMap<Integer, Movie> movieDatabase;
+    private static int nextId = 1;
+
+    public MovieDatabase() {
+    }
+
+    public void addMovie(Movie movie) {
+        movieDatabase.put(nextId, movie);
+        nextId ++;
+    }
+
+    public void
 }
