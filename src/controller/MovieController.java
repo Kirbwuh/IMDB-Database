00e60432b3abf5ad2b9bef7be4218eab08c85067
Arraf-Hoque <101687776+Arraf-Hoque@users.noreply.github.com>
2026-03-23 11:27:58 -0600

    sticking dukus code into mine #stealing

--- src/controller/MovieController.java
+++ src/controller/MovieController.java
@@ -2,6 +2,13 @@ package src.controller;
 
 public class    MovieController {
 
+    /*
+    gonna write the basic structure of movie controller before i talk to duku about stuff
+     */
+
+
+    // calling MovieDatabase
+
 
 
 }
