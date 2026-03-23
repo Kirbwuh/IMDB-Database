369cad4da01b3964d4418557de5ef636d977540e
Arraf-Hoque <101687776+Arraf-Hoque@users.noreply.github.com>
2026-03-23 15:41:20 -0600

    i think im stupid but i got it now LOLOLOLOL

--- src/controller/MovieController.java
+++ src/controller/MovieController.java
@@ -1,7 +1,9 @@
 package src.controller;
 import Model.MovieDatabase;
 import src.view.InputView;
-import src.model.Movie;
+import model.Movie;
+
+
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,12 +12,27 @@ import java.util.Scanner;
 
 
 
-public class    MovieController {
+public class  MovieController{
 
     private static final Model.MovieDatabase MD = new MovieDatabase();
     private static Scanner scanner;
 
 
+
+
+    private static Object stringToMovie(ArrayList<String> data){
+            Movie movie = new Movie(
+                    data.get(0),                         // title
+                    Integer.parseInt(data.get(1)),       // year
+                    Boolean.parseBoolean(data.get(2)),                         // genre
+                    data.get(3),     // rating
+                    Double.parseDouble(data.get(4)),                         // description
+                    data.get(5),                         // director
+                    data.get(6),
+                    Long.parseLong(data.get(7)));
+
+            return movie;
+    }
 
     // calling MovieDatabase
     private static void addMovieGetter(String choice){
