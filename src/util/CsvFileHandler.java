0032e4daf1ef14d05309b1036f36c7e6518fccc2
Delsune <deltarad.theta@gmai.com>
2026-03-23 16:00:38 -0600

    Finished CsvFileHandler.java by extending Movie.java to append toCSVStringRow to file.

--- src/util/CsvFileHandler.java
+++ src/util/CsvFileHandler.java
@@ -6,20 +6,28 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 /**
- * Handles saving and loading of comma separated value files.
+ * Handles saving and loading of comma separated value files. Extends from Movie.java to use the toCSVStringRow method.
  * @author Haitam Lembaid 18/03/2026 T10
  */
-public class CsvFileHandler {
+public class CsvFileHandler extends model.Movie {
     // Instance Variables
     private final String filepath;
     private final File movies;
 
     /**
-     * The defined constructor for CSVFileHandler.
-     * @param filepath - The relative filepath of the csv file.
-     * @param movies - The csv file name.
+     * Constructor built from extending Movie.java, with filepath and movies instance variables added in.
+     * @param title         movie title
+     * @param year          release year
+     * @param certification true if rated PG-13
+     * @param genre         genre(s) of the movie
+     * @param imdbRating    IMDB rating out of 10
+     * @param description   short description of the movie
+     * @param director      name of the director
+     * @param gross         total box office gross earnings
      */
-    public CsvFileHandler(String filepath, File movies) {
+    public CsvFileHandler(String title, int year, boolean certification, String genre, double imdbRating,
+                          String description, String director, long gross, String filepath, File movies) {
+        super(title, year, certification, genre, imdbRating, description, director, gross);
         this.filepath = filepath;
         this.movies = movies;
     }
@@ -31,7 +39,8 @@ public class CsvFileHandler {
     public void saveToCSV(String filepath) {
         try {
             FileWriter save = new FileWriter(filepath);
-            save.append();
+            save.append(toCSVStringRow());
+            save.close();
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("The file is not in the same directory (either doesn't exist or was moved.)");
