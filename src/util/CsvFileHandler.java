b48303b5712fd9fd1b0d9eeed14b6f3da823bc93
Delsune <deltarad.theta@gmai.com>
2026-03-23 10:12:39 -0600

    Worked on saving to csv and loading csv. Also added javadocs. Still needs work.

--- src/util/CsvFileHandler.java
+++ src/util/CsvFileHandler.java
@@ -1,8 +1,9 @@
 package src.util;
 
-import org.w3c.dom.css.CSSValue;
-
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Handles saving and loading of comma separated value files.
@@ -11,22 +12,47 @@ import java.io.File;
 public class CsvFileHandler {
     // Instance Variables
     private final String filepath;
-    private Model.MovieDatabase movieEntry;
-    private File movies;
+    private final File movies;
 
-    public CsvFileHandler(String filepath, Model.MovieDatabase movieEntry, File movies) {
+    /**
+     * The defined constructor for CSVFileHandler.
+     * @param filepath - The relative filepath of the csv file.
+     * @param movies - The csv file titled "Movie.csv".
+     */
+    public CsvFileHandler(String filepath, File movies) {
         this.filepath = "src/util/Movies.csv";
-        this.movieEntry = new Model.MovieDatabase();
         this.movies = new File("Movies.csv");
     }
 
     /**
-     *
-     * @param filepath
-     * @param db
+     * Method to save added movies to CSV file. Catches IOException.
+     * @param filepath - The relative filepath of the csv file.
+     * @param movies - The csv file titled "Movie.csv".
      */
-    public void saveToCSV(String filepath, Model.MovieDatabase db) {
+    public void saveToCSV(String filepath, File movies) {
+        try {
+            FileWriter save = new FileWriter(filepath);
+            save.append();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("The file is not in the same directory (either doesn't exist or was moved.)");
+        }
+    }
 
+    /**
+     * Loads the csv file into memory for reading contents.
+     * @param filepath - The relative filepath of the csv file.
+     * @param movies - The csv file titled "Movie.csv".
+     */
+    public void loadCSV(String filepath, File movies) {
+        try {
+            FileReader load = new FileReader(filepath);
+            load.readAllLines();
+            load.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("The file is not in the same directory (either doesn't exist or was moved.");
+        }
     }
 
     /**
