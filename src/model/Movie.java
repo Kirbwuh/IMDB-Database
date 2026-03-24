f729514dadafa59c64c64e73a460ed6a70117957
J.J. Rondon <82114258+Joaron4@users.noreply.github.com>
2026-03-23 19:02:28 -0600

    Refactor to string csv methods in movie and series

--- src/model/Movie.java
+++ src/model/Movie.java
@@ -110,7 +110,7 @@ public class Movie extends src.model.RowEntry {
      */
     @Override
     public String toCSVStringRow() {
-        return super.toCSVStringRow()    // id,title,year,genre,imdbRating,description
+        return super.commonCSVFields()    // id,title,year,genre,imdbRating,description
                 + "," + certification
                 + "," + director
                 + "," + gross;
