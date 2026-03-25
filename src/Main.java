9260d1e4535300e29c99803e2c247935ed73e902
J.J. Rondon <82114258+Joaron4@users.noreply.github.com>
2026-03-25 14:14:49 -0600

    Join all views in main
    Signed-off-by: J.J. Rondon <82114258+Joaron4@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,16 @@
-static void main(String[] args) {
-    
+package src;
+
+import java.util.Scanner;
+import src.view.ConsoleView;
+import src.view.InputView;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		ConsoleView consoleView = new ConsoleView();
+		InputView inputView = new InputView(scanner);
+
+		consoleView.printMainMenu(scanner);
+		InputView.showMainMenu(scanner);
+	}
 }
