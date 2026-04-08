021a27beb6880033fa3fb297d00a0480f028b8e4
J.J. Rondon <82114258+Joaron4@users.noreply.github.com>
2026-04-08 11:45:12 -0600

    Implemented mainView and added skeleton to main controller
    Signed-off-by: J.J. Rondon <82114258+Joaron4@users.noreply.github.com>

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,20 +1,143 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
-import javafx.fxml.Initializable;
-import java.net.URL;
-import java.util.ResourceBundle;
+public class MainController {
 
-/**
- * MainController.java
- * JavaFX controller for MainView.fxml
- * @author J.J. Rondon 07/04/2026 T10
- */
-public class MainController implements Initializable {
+    @FXML
+    private Button addMovieCenterBtn;
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        // runs automatically when the FXML loads
-        System.out.println("MainController initialized");
+    @FXML
+    private TableColumn<?, ?> description;
+
+    @FXML
+    private Button editMovieBtn;
+
+    @FXML
+    private TableColumn<?, ?> genreCol;
+
+    @FXML
+    private ComboBox<?> genreComboBox;
+
+    @FXML
+    private Button highestRatedBtn;
+
+    @FXML
+    private TableView<?> highlightsTableView;
+
+    @FXML
+    private TableColumn<?, ?> hlMovieCol;
+
+    @FXML
+    private TableColumn<?, ?> hlRatingCol;
+
+    @FXML
+    private Label infoCertLabel;
+
+    @FXML
+    private Label infoDescLabel;
+
+    @FXML
+    private Label infoDirectorLabel;
+
+    @FXML
+    private Label infoGenreLabel;
+
+    @FXML
+    private Label infoGrossLabel;
+
+    @FXML
+    private Label infoRatingLabel;
+
+    @FXML
+    private Label infoTitleLabel;
+
+    @FXML
+    private Label infoYearLabel;
+
+    @FXML
+    private Button loadCSVBtn;
+
+    @FXML
+    private Button lowestRatedBtn;
+
+    @FXML
+    private TableView<?> moviesTableView;
+
+    @FXML
+    private TableColumn<?, ?> ratingCol;
+
+    @FXML
+    private Button removeMovieBtn;
+
+    @FXML
+    private Button saveCSVBtn;
+
+    @FXML
+    private Button searchBtn;
+
+    @FXML
+    private TextField searchField;
+
+    @FXML
+    private TableColumn<?, ?> titleCol;
+
+    @FXML
+    private Button top5Btn;
+
+    @FXML
+    private TableColumn<?, ?> yearCol;
+
+    @FXML
+    void handleAddMovie(ActionEvent event) {
+
     }
+
+    @FXML
+    void handleEditMovie(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleHighestRated(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleLoadCSV(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleLowestRated(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleRemoveMovie(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSaveCSV(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSearch(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleTop5(ActionEvent event) {
+
+    }
+
 }
