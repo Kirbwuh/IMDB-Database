b614cf54490f838e56ffb9dc3effa4b10e017a83
J.J. Rondon <82114258+Joaron4@users.noreply.github.com>
2026-04-17 10:11:37 -0600

    Implemented laoding with command line and not starting with loaded csv
    Signed-off-by: J.J. Rondon <82114258+Joaron4@users.noreply.github.com>

--- src/main/java/main/java/Main.java
+++ src/main/java/main/java/Main.java
@@ -1,9 +1,12 @@
 package main.java;
 
+import controller.MainController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.util.List;
 
 /**
  * Main.java
@@ -20,6 +23,14 @@ public class Main extends Application {
 				getClass().getResource("/view/MainView.fxml")
 		);
 		Scene scene = new Scene(loader.load(), 900, 600);
+
+		MainController mainController = loader.getController();
+		List<String> args = getParameters().getRaw();
+		if (args.contains("--load")) {
+			mainController.setLoadCsvOnStartup(true);
+		}
+		mainController.initializeStartupData();
+
 		primaryStage.setTitle("CPSC 219 - Movie & Series Database");
 		primaryStage.setScene(scene);
 		primaryStage.show();
